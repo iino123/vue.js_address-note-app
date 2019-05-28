694e68c0fc5afcc2bfeb0dc759b444b4aaf32362
iino123 <zxcvbnm.ken@gmail.com>
2019-05-28 17:11:39 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-side-icon @click="openSideMenu"></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>アドレス帳</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <SideNav/>
+
+    <v-content>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import SideNav from './components/SideNav.vue'
+export default {
+  name: 'App',
+  components: {
+    SideNav
+  },
+  data () {
+    return {
+      //
+    }
+  },
+  methods: {
+    openSideMenu () {
+      this.$store.dispatch('toggleSideMenu')
+    }
+  }
+}
+</script>
