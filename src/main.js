20a9577c74d13a4124e2bdb4b654f1d8779e8d90
iino123 <zxcvbnm.ken@gmail.com>
2019-05-28 21:55:41 +0900

    setup firebase & add AddressForm.vue

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyC-hOBE65KSxn69eBp8TK0c69HCY_3Ihus",
+    authDomain: "my-address-pj-a4659.firebaseapp.com",
+    databaseURL: "https://my-address-pj-a4659.firebaseio.com",
+    projectId: "my-address-pj-a4659",
+    storageBucket: "my-address-pj-a4659.appspot.com",
+    messagingSenderId: "1028998214531",
+    appId: "1:1028998214531:web:36ff2711e99befd0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
